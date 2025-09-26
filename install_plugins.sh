840f019cac1701fdb2c68f59601209e2d5308968
Terry Zhao <zhao.terryy@gmail.com>
2025-09-26 18:31:45 -0400

    fix for 9/17 patch

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,9 +3,9 @@
 cd $HOME
 
 CSGO_DIRECTORY=/home/steam/cs2-dedicated/game/csgo
-METAMOD_URL=https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1293-linux.tar.gz
-CSSHARP_URL=https://github.com/roflmuffin/CounterStrikeSharp/releases/download/v242/counterstrikesharp-with-runtime-build-242-linux-2eaf7c2.zip
-MATCHZY_URL=https://github.com/shobhit-pathak/MatchZy/releases/download/0.7.11/MatchZy-0.7.11.zip
+METAMOD_URL=https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1366-linux.tar.gz
+CSSHARP_URL=https://github.com/roflmuffin/CounterStrikeSharp/releases/download/v1.0.340/counterstrikesharp-with-runtime-linux-1.0.340.zip
+MATCHZY_URL=https://github.com/shobhit-pathak/MatchZy/releases/download/0.8.13/MatchZy-0.8.13.zip
 
 # https://shobhit-pathak.github.io/MatchZy/installation/
 # download metamod & extract to ./metamod
--- pre.sh
+++ pre.sh
@@ -8,4 +8,7 @@ CSGO_DIRECTORY=/home/steam/cs2-dedicated/game/csgo
 
 cp -f admins.json $CSGO_DIRECTORY/addons/counterstrikesharp/configs/admins.json
 cp -f matchzy_config.cfg $CSGO_DIRECTORY/cfg/MatchZy/config.cfg
+cp -f autoexec.cfg $CSGO_DIRECTORY/cfg/autoexec.cfg
 
+# switch back to server directory before exiting script
+cd $STEAMAPPDIR/game/bin/linuxsteamrt64
